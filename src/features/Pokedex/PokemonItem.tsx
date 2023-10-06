35857bcdd511385890ad6e8e563f8ac184d478a6
Leonard Krusch <Leonard.Krusch@aclue.de>
2023-10-06 13:07:28 +0200

    Alt Text added to Pokedex Items

--- src/features/Pokedex/PokemonItem.tsx
+++ src/features/Pokedex/PokemonItem.tsx
@@ -7,7 +7,7 @@ export const PokemonItem: React.FC<BasePokemon> = ({id, imageSrc, name, types}) 
         <Link to={`pokemon/${name}`} className={styles.card}>
             <div className={styles.item}>
                 <div className={styles.id}>#{id}</div>
-                <div className="image"><img src={imageSrc} /></div>
+                <div className="image"><img src={imageSrc} alt={`Image of ${name}`} /></div>
                 <div className={styles.name}>{name}</div>
                 <div className={styles.types}>{types.join(', ')}</div>
             </div>
